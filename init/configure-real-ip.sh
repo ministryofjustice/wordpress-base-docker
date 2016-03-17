8336c4418de7bd69286c0c011d0d3e4988b2de07
Jonathan Conway <jonathan@jaikoo.com>
2016-03-17 09:33:18 +0000

    Move Real IP config to its own file

---
+++ init/configure-real-ip.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#######
+# As WP is and should be served through a load balancer for a HA setup, the IP address
+# that comes through will be that of the LB. In order to get around this we use the
+# RealIP module
+######
+
+# If LB_IP_RANGE env var containing the IP range for the load balancers is set, user the Real IP Nginx module
+
+if [ ! -z "$LB_IP_RANGE" ]
+then
+	echo "#Set trusted sources that can set the RealIP e.g loadbalancer"
+	echo "set_real_ip_from ${LB_IP_RANGE};" > /etc/nginx/real_ip.conf
+	echo "real_ip_header X-Forwarded-For;" >> /etc/nginx/real_ip.conf
+	echo "real_ip_recursive on;" >> /etc/nginx/real_ip.conf
+fi
