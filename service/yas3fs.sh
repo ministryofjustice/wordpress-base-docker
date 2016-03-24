e1cd4cddd3468484977aced78dc14ac37ac83a3b
Jonathan Conway <jonathan@jaikoo.com>
2016-03-24 10:44:12 +0000

    Enable SNS/SQS synch for Yas3fs

--- service/yas3fs.sh
+++ service/yas3fs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Mount the WordPress uploads directory to S3 using yas3fs
-exec /usr/local/bin/yas3fs --foreground --no-metadata --nonempty s3://$AWS_S3_BUCKET/uploads /bedrock/web/app/uploads
+exec /usr/local/bin/yas3fs --foreground --no-metadata --nonempty s3://$AWS_S3_BUCKET/uploads /bedrock/web/app/uploads --topic $SNS_TOPIC --queue $SQS_QUEUE
