6b7ed6b974f0746fd39500770b601f6c1ed284cd
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-09-01 12:01:53 +0100

    Adjust the way nullmailer's trigger files are generated to resolve a bug where multiple instances of `nullmailer-send` would run.

--- service/nullmailer.sh
+++ service/nullmailer.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# First start nullmailer via its /etc/init.d script
-# This feels a bit hacky, but what we want nullmailer to create its required trigger files
-# We don't want it running as a daemon, so we'll kill it as soon as it starts
-/etc/init.d/nullmailer start
-/etc/init.d/nullmailer stop
+# Create the nullmailer trigger file if it doesn't already exist
+# Nullmailer will not start without this
+# Taken from the `start` sequence in /etc/init.d/nullmailer
+
+if [ ! -p /var/spool/nullmailer/trigger ]; then
+    rm -f /var/spool/nullmailer/trigger
+    mkfifo /var/spool/nullmailer/trigger
+fi
+
+chown mail:root /var/spool/nullmailer/trigger
+chmod 0622 /var/spool/nullmailer/trigger
 
 # Then run it in the foreground
-exec /sbin/setuser mail /usr/sbin/nullmailer-send -s
+exec /sbin/setuser mail /usr/sbin/nullmailer-send
