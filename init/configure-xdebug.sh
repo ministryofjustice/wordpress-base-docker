cc93458fa6dd413ca7ec459681e8aa92865021dc
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-09-06 16:40:22 +0100

    Add comment to `configure-xdebug` init script for future clarity.

--- init/configure-xdebug.sh
+++ init/configure-xdebug.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Enable XDebug if XDEBUG_CONFIG environment variable exists
+# XDebug will automatically read config from XDEBUG_CONFIG, so all we need to do here is enable it.
 
 if [ ! -z "$XDEBUG_CONFIG" ]
 then
