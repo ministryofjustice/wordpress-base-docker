4467e05ea85f315b3bf0524a8c22ba3a4f3d9af3
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2020-02-24 17:01:52 +0000

    Add security-txt script

---
+++ init/configure-security-txt.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# create publicly accessible security.txt file
+curl -s https://raw.githubusercontent.com/ministryofjustice/security-guidance/master/contact/vulnerability-disclosure-security.txt -o /tmp/security.txt
+mkdir /bedrock/web/.well-known
+cp /tmp/security.txt /bedrock/web/.well-known/security.txt
