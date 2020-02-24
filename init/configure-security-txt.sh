1c07221286626039c0780669b469baf0ac921824
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2020-02-24 17:37:14 +0000

    Only execute if the bedrock directory exists

--- init/configure-security-txt.sh
+++ init/configure-security-txt.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 # create publicly accessible security.txt file
-curl -s https://raw.githubusercontent.com/ministryofjustice/security-guidance/master/contact/vulnerability-disclosure-security.txt -o /tmp/security.txt
-mkdir /bedrock/web/.well-known
-cp /tmp/security.txt /bedrock/web/.well-known/security.txt
+# copies the entire text from the working file; vulnerability-disclosure-security.txt
+if [ -d "/bedrock" ]
+then
+  if [ ! -d "/bedrock/web/.well-known" ]
+  then
+    mkdir /bedrock/web/.well-known
+  fi
+
+  curl -s https://raw.githubusercontent.com/ministryofjustice/security-guidance/master/contact/vulnerability-disclosure-security.txt -o /bedrock/web/.well-known/security.txt
+fi
