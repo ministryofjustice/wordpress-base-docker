5845c3ce0a316f33faf03ebeeff087b3b7f6042c
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2020-02-24 22:37:23 +0000

    Implement 301 redirect

--- init/configure-security-txt.sh
+++ init/configure-security-txt.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 # create publicly accessible security.txt file
-# copies the entire text from the working file; vulnerability-disclosure-security.txt
-if [ -d "/bedrock" ]
-then
-  if [ ! -d "/bedrock/web/.well-known" ]
-  then
-    mkdir /bedrock/web/.well-known
-  fi
-
-  curl -s https://raw.githubusercontent.com/ministryofjustice/security-guidance/master/contact/vulnerability-disclosure-security.txt -o /bedrock/web/.well-known/security.txt
-fi
+#curl -s https://raw.githubusercontent.com/ministryofjustice/security-guidance/master/contact/vulnerability-disclosure-security.txt -o /tmp/security.txt
+#mkdir /bedrock/web/.well-known
+#mv /tmp/security.txt /bedrock/web/.well-known/security.txt
